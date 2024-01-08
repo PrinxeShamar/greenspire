12160f0323e0e0b169dba697e615e4931cd68770
PrinxeShamar <shamarsamuelsbusiness@gmail.com>
2024-01-07 20:57:35 -0500

    Push

--- api/index.py
+++ api/index.py
@@ -1,9 +1,44 @@
 from flask import Flask
 from flask_cors import CORS
+from moviepy.editor import *
+from gtts import gTTS
+import time
+from supabase import create_client, Client
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/index")
-def index():
-    return ['one', 'two', 'three']
+def get_tts_audio_clip(text, file_name):
+    tts_clip = gTTS(text, slow=False)
+    tts_clip.save(file_name)
+    audio_clip = AudioFileClip(file_name)
+    return audio_clip
+
+@app.route("/api/problems/<id>")
+def index(id):
+    supabase: Client = create_client("https://hpvhyormhqzyycknztly.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhwdmh5b3JtaHF6eXlja256dGx5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNDU1Mzg5NiwiZXhwIjoyMDIwMTI5ODk2fQ.gjQpluE78qTAYksDEkmLiW4Xa7_-vCQhOlUP3dEkiy8")
+    response = supabase.table('test').select("*").eq('id', id).execute()
+    
+    problem = response.data[0]['gpt']['Summarized Problem']
+    solution = response.data[0]['gpt']['Summarized Solution']
+
+    problem_audio = get_tts_audio_clip(problem, f"{id}-problem.wav")
+    problem_text_clip = TextClip(problem, bg_color='yellow', color='green', size = (360, 640), method='caption').set_duration(problem_audio.duration).set_pos('center').set_start(1)
+    problem_background = (ImageClip("./api/green.png").set_duration(problem_audio.duration + 2)).set_audio(problem_audio.set_start(1))
+    part1 = CompositeVideoClip([problem_background, problem_text_clip])
+
+    solution_audio = get_tts_audio_clip(solution, f"{id}-solution.wav")
+    solution_text_clip = TextClip(solution, bg_color='green', color='yellow', size = (360, 640), method='caption').set_duration(solution_audio.duration).set_start(1).set_pos('center')
+    solution_background = (ImageClip("./api/yellow.png").set_duration(solution_audio.duration + 2)).set_audio(solution_audio.set_start(1))
+    part2 = CompositeVideoClip([solution_background, solution_text_clip])
+
+    promo_background = (ImageClip("./api/promo.png").set_duration(1))
+    part3 = CompositeVideoClip([promo_background])
+
+    result = concatenate_videoclips([part1, part2, part3])
+    result.write_videofile(f"./{id}-output.mp4", fps=1)
+
+    with open(f"{id}-output.mp4", 'rb') as f:
+        supabase.storage.from_("videos").upload(file=f, path=f"{id}-output.mp4", file_options={"content-type": "video/mp4"})
+
+    return f"{id}-output.mp4"
--- src/app/problems/[id]/view.js
+++ src/app/problems/[id]/view.js
@@ -68,6 +68,12 @@ export default function View({ problem }) {
 
   return (
     <div className="flex-1 flex flex-col text-xs gap-2">
+      <video height="200px" width="200px" controls>
+        <source
+          src={`https://hpvhyormhqzyycknztly.supabase.co/storage/v1/object/public/videos/${problem.id}-output.mp4`}
+          type="video/mp4"
+        />
+      </video>
       <div className="flex flex-col">
         <div className="font-black text-green-400">Problem</div>
         <div className="">{problem.problem}</div>
@@ -80,33 +86,39 @@ export default function View({ problem }) {
         <div className="font-black text-green-400">Metric Analysis</div>
         <div className="flex gap-4 justify-center my-4">
           {Object.entries(problem.gpt).map(([metric, value], index) => {
-            if (metric === "Industry") return;
-            let total = value.After + value.Before;
-            return (
-              <div key={index} className="flex flex-col items-center gap-4">
-                <div className="font-black">{metric}</div>
-                <div className="flex h-32 w-32 border-b-2 border-black items-end">
-                  <div className="flex-grow" />
-                  <div
-                    className={`w-6 bg-red-400`}
-                    style={{
-                      height: `${(value.Before / total) * 100}%`,
-                    }}
-                  ></div>
-                  <div className="flex-grow" />
-                  <div
-                    className={`w-6 h-1/5 bg-blue-400`}
-                    style={{
-                      height: `${(value.After / total) * 100}%`,
-                    }}
-                  ></div>
-                  <div className="flex-grow" />
+            if (
+              metric === "Water Use" ||
+              metric === "Waste Generation" ||
+              metric === "Energy-Generation Mix" ||
+              metric === "Greenhouse Gas Emissions"
+            ) {
+              let total = value.After + value.Before;
+              return (
+                <div key={index} className="flex flex-col items-center gap-4">
+                  <div className="font-black">{metric}</div>
+                  <div className="flex h-32 w-32 border-b-2 border-black items-end">
+                    <div className="flex-grow" />
+                    <div
+                      className={`w-6 bg-red-400`}
+                      style={{
+                        height: `${(value.Before / total) * 100}%`,
+                      }}
+                    ></div>
+                    <div className="flex-grow" />
+                    <div
+                      className={`w-6 h-1/5 bg-blue-400`}
+                      style={{
+                        height: `${(value.After / total) * 100}%`,
+                      }}
+                    ></div>
+                    <div className="flex-grow" />
+                  </div>
+                  <div className="text-[10px] text-center w-64">
+                    {value.Reason}
+                  </div>
                 </div>
-                <div className="text-[10px] text-center w-64">
-                  {value.Reason}
-                </div>
-              </div>
-            );
+              );
+            }
           })}
         </div>
       </div>
